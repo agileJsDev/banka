532a1ecf3c1d0cdfca17c2bcc4c24243435eeaa1
Ayodeji <afolabiayodejia@gmail.com>
2019-03-27 02:09:52 +0100

    New Feature- Transaction page

--- ui/js/main.js
+++ ui/js/main.js
@@ -3,12 +3,21 @@ const mobileMenu = document.querySelector(".mobile__top-nav");
 const passwordDOM = document.querySelector("#password")
 const comfirmPasswordDom = document.querySelector("#confirm_password");
 const grid = document.querySelector(".grid-container");
+const viewDetailsBtnDOM = document.querySelectorAll('.view__btn');
+const divi = document.querySelector('.divi')
 
+const createNode = (element, content) => {
+	const el = document.createElement(element);
+	el.textContent = content;
+	return el;
+}
+
+const appendTo = (parent, child) => parent.appendChild(child);
 
 /* Mobile Menu Functionality*/
 xBtn.addEventListener('click', () => {
-	if ( mobileMenu) {
-	mobileMenu.style.display === "block" ? mobileMenu.style.display = "none" : mobileMenu.style.display = "block";
+	if (mobileMenu) {
+		mobileMenu.style.display === "block" ? mobileMenu.style.display = "none" : mobileMenu.style.display = "block";
 	}
 	if (grid) {
 		grid.classList.toggle("dropdown__active")
@@ -18,13 +27,49 @@ xBtn.addEventListener('click', () => {
 
 /* Front-end Password Confirmation */
 const confirmPassword = () => {
-	if ( passwordDOM.value === comfirmPasswordDom.value ) {
+	if (passwordDOM.value === comfirmPasswordDom.value) {
 		comfirmPasswordDom.setCustomValidity('');
 	} else {
 		comfirmPasswordDom.setCustomValidity("Passwords don't match.");
 	}
 }
 if (passwordDOM) {
-	passwordDOM.oninput = confirmPassword;comfirmPasswordDom.oninput = confirmPassword;
+	passwordDOM.oninput = confirmPassword; comfirmPasswordDom.oninput = confirmPassword;
 };
+
+const dialog = () => {
+	viewDetailsBtnDOM.forEach(btnDOM => {
+		const dialog = document.querySelector('.dialog');
+		const close = document.querySelector('.close');
+
+		btnDOM.addEventListener('click', () => {
+			const transacDetail = btnDOM.parentElement.parentNode;
+			const detail = {
+				date: transacDetail.querySelector('.transac_date').innerHTML,
+				desc: transacDetail.querySelector('.transac_details').innerHTML,
+				amount: transacDetail.querySelector('.transac_amount').innerHTML
+			};
+			const h1Tag = createNode('h1', detail.date);
+			const para = createNode('p', detail.desc);
+			const amountTag = createNode('h1', `NGN ${detail.amount}`);
+			const parentNode =  createNode('div', '')
+			appendTo(parentNode, h1Tag);
+			appendTo(parentNode, para);
+			appendTo(parentNode, amountTag);
+			divi.appendChild(parentNode)
+			dialog.classList.add('hidden');
+			const arry = [close, dialog]
+			arry.forEach( dom => {
+				dom.addEventListener('click', () => {
+					dialog.classList.remove('hidden');
+					while(divi.firstChild) {
+						divi.removeChild(divi.firstChild)
+					}
+				});
+			})
+
+		});
+	});
+};
+if (viewDetailsBtnDOM) dialog();
 
