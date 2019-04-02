506a4c5b44f9cd31670e5cea5be01f1b9098f215
Ayodeji <afolabiayodejia@gmail.com>
2019-04-02 01:59:13 +0100

    Setup-Unit-Testing

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome To Banka Default Route');
-});
+app.get('/', (req, res) => res.status(200).json({
+  status: res.statusCode,
+  data: 'Welcome to Banka Default'
+}));
 
 const port = process.env.PORT || 9000;
 app.listen(port, console.log('Server running...!!!'));
