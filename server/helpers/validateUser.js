a5649008f954d20c23f042bf8792a74c8494b4ec
Ayodeji <afolabiayodejia@gmail.com>
2019-04-10 08:21:50 +0100

    ft - API endpoint to create bank account

--- server/helpers/validateUser.js
+++ server/helpers/validateUser.js
@@ -38,45 +38,6 @@ const logInSchema = {
 const accountRegSchema = {
   type: name
 };
-
-// const validateWithSchenma = (data, inputSchema) => {
-//   const schema = inputSchema;
-//   return Joi.validate(data, schema);
-// };
-
-/* const validateAccountReg = (req, res, next) => {
-  const { error } = validateWithSchenma(req.body, accountRegSchema);
-  if (error) {
-    return res.status(400).json({
-      status: res.statusCode,
-      error: error.details[0].message
-    });
-  }
-  return next();
-};
-
-const validateSignUp = (req, res, next) => {
-  const { error } = validateWithSchenma(req.body, signUpScheama);
-  if (error) {
-    return res.status(400).json({
-      status: res.statusCode,
-      error: error.details[0].message
-    });
-  }
-  return next();
-};
-
-const validateLogIn = (req, res, next) => {
-  const { error } = validateWithSchenma(req.body, logInSchema);
-  if (error) {
-    return res.status(400).json({
-      status: res.statusCode,
-      error: error.details[0].message
-    });
-  }
-  return next();
-}; */
-
 const validate = (schema) => {
   const validateInput = (req, res, next) => {
     const { error } = Joi.validate(req.body, schema);
