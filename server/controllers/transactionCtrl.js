9308077e8ba83c8438965b3c805a05ac6556e31f
Ayodeji <afolabiayodejia@gmail.com>
2019-04-12 19:19:00 +0100

    ft-#165301768-API endpoint to credit account

--- server/controllers/transactionCtrl.js
+++ server/controllers/transactionCtrl.js
@@ -40,6 +40,40 @@ class transactionsCtrl {
       return next(err);
     }
   }
+
+  static async credit(req, res, next) {
+    try {
+      const cashier = req.user.id;
+      const account = accountModel.findAccountByNo(req.params.accountNumber);
+      if (!account) {
+        return res.status(404).json({
+          status: res.statusCode, error: 'Account does not exist'
+        });
+      }
+
+      const oldBalance = account.balance;
+      const amount = Number(req.body.amount);
+      const newBalance = await accountModel.credit(account.accountNumber, amount);
+      const data = {
+        type: 'credit', oldBalance, amount, newBalance, accountNumber: account.accountNumber, cashier
+      };
+      const transactionDetail = tModel.create(data);
+      return res.status(201).json({
+        status: res.statusCode,
+        data: {
+          transactionId: transactionDetail.id,
+          accountNumber: transactionDetail.accountNumber,
+          amount: transactionDetail.amount,
+          cashier: transactionDetail.cashier,
+          transactionType: transactionDetail.type,
+          acccountBalance: transactionDetail.newBalance,
+          date: transactionDetail.createdOn
+        }
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }
 }
 
 export default transactionsCtrl;
--- tests/06_debit_account.spec.js
+++ tests/06_debit_account.spec.js
@@ -68,3 +68,55 @@ describe('Staff(Cashier) should be able to debit bank account', () => {
     });
   });
 });
+
+
+// Credit Account API Endpoint Test
+
+describe('Staff(Cashier) should be able to credit bank account', () => {
+  describe('POST /api/v1/transactions/<account_number>/credit', () => {
+    describe("When Staff(Cashier) wants to credit a user's bank account", () => {
+      before(async () => {
+        adminToken = userModel.generateAuthToken(
+          { id: 1, type: 'staff', isAdmin: false }
+        );
+        userToken = userModel.generateAuthToken(
+          { id: 1, type: 'client', isAdmin: false }
+        );
+      });
+
+      // Throw error 403 if unauthorized user tries to credit user bank account
+      it('Send unauthorized message if user is not authorized', async () => {
+        const res = await chai.request(app).post(`/api/v1/transactions/${resp.body.data.accountNumber}/credit`).set('Authorization', userToken).send({ amount: '400' });
+        expect(res).to.have.status(403);
+        expect(res.body).to.have.property('error').to.deep.equal('Unauthorized');
+      });
+
+      // Respond with error 404 if account number to be credited does not exist
+      it('should respond with error 404 - not found - if account number does not exist', async () => {
+        const res = await chai.request(app).post('/api/v1/transactions/1234567809/credit').set('Authorization', adminToken).send({ amount: '400' });
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property('error');
+      });
+
+
+      // Get an error response if token is invalid or expired - status code 401
+      it('should respond with error 401 - unauthorized - if token is invalid or expired', async () => {
+        const res = await chai.request(app).post(`/api/v1/transactions/${resp.body.data.accountNumber}/credit`).set('Authorization', 'eyJhbGciOiJIUzI1NiIsIkpXVCJ9.kcvIkS9ACezPO2DgAi-XvEikLy9ZA2y0kWiQ').send({ amount: '400' });
+        expect(res).to.have.status(401);
+        expect(res.body).to.have.property('error').to.deep.equal('Invalid Token');
+      });
+
+      // Get 201 status code if account is successfully credited
+      it('should respond with a status code 201 if account is successfully credited', async () => {
+        const res = await chai.request(app).post(`/api/v1/transactions/${resp.body.data.accountNumber}/credit`).set('Authorization', adminToken).send({ amount: '1700' });
+        expect(res).to.have.status(201);
+        expect(res.body.data).to.have.property('transactionId');
+        expect(res.body.data).to.have.property('accountNumber');
+        expect(res.body.data).to.have.property('amount');
+        expect(res.body.data).to.have.property('cashier');
+        expect(res.body.data).to.have.property('transactionType');
+        expect(res.body.data).to.have.property('acccountBalance').to.deep.equal(2000);
+      });
+    });
+  });
+});
